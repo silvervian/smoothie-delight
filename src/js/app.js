e1f6043eb740d0f08480d59bee353c7d16287836
Adijos <jedrczakadrian@gmail.com>
2019-03-02 20:30:09 +0100

    Add smoothScroll between anchors

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 import scrollUp from './scrollUp';
+import smoothScroll from './smoothScroll';
 
 scrollUp();
+smoothScroll();
---
+++ src/js/smoothScroll.js
@@ -0,0 +1,16 @@
+function init() {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const empty = anchor.getAttribute('href');
+      if (empty === '#') return;
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+}
+
+export default init;
